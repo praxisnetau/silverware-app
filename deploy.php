ea15996e8fb36e80eaa1a424f460bb6e005aa5a5
Colin Tucker <colin@praxis.net.au>
2017-10-11 22:55:03 +1100

    Updated SilverStripe helper tasks for deployment

--- deploy.php
+++ deploy.php
@@ -48,11 +48,11 @@ set('writable_dirs', [
 // Define SilverStripe Helper Tasks:
 
 task('silverstripe:build', function () {
-    return run('{{bin/php}} {{release_path}}/framework/cli-script.php dev/build');
+    return run('{{bin/php}} {{release_path}}/vendor/silverstripe/framework/cli-script.php dev/build');
 })->desc('Run /dev/build');
 
 task('silverstripe:buildflush', function () {
-    return run('{{bin/php}} {{release_path}}/framework/cli-script.php dev/build flush=all');
+    return run('{{bin/php}} {{release_path}}/vendor/silverstripe/framework/cli-script.php dev/build flush=all');
 })->desc('Run /dev/build?flush=all');
 
 // Define Deployment Task:
